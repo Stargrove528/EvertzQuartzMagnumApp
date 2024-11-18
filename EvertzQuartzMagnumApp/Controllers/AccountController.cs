e8948cf8c2b67fa945372fe8b3bc15bb82032747
Stargrove528 <stargrove@stargrove.net>
2024-11-18 16:29:35 -0700

    Add project files.

---
+++ EvertzQuartzMagnumApp/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class AccountController : Controller
+{
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(string username, string password)
+    {
+        if (username == "admin" && password == "password") // Example logic
+        {
+            return RedirectToAction("Index", "Switching");
+        }
+
+        ModelState.AddModelError("", "Invalid username or password.");
+        return View();
+    }
+
+}
